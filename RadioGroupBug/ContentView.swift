b7f629d3462a8472c34366e3d55b721c57e31d43
Richard Legault <rlegault33@yahoo.ca>
2020-11-08 18:20:52 -0500

    Initial Commit

---
+++ RadioGroupBug/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RadioGroupBug
+//
+//  Created by Richard Legault on 2020-11-08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
